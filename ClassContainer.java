d005e693de1a5f487e027eb83931bf5c2f427850
Cullenisnthere <cullen@kurtzpad.com>
2024-01-31 14:21:42 -0500

    Added a clarifying comment and a placeholder for rename class.

--- ClassContainer.java
+++ ClassContainer.java
@@ -28,18 +28,24 @@ public class ClassContainer
         //Class was added with no issues.
         return "Class " + newClass.getName() + " was added.";
     }
+    //Doesn't yet deal with classes with relationships!!!
+    //Going to wait until relationship stuff is made to do that.
     public String removeClass(ClassBase remClass)
     {
-    for(ClassBase classList : Classes)
-    {
+        for(ClassBase classList : Classes)
+        {
         //Loops through every class in Classes, checking if any match
         //the class being removed. If so, it removes it.
-        if(classList.getName().equals(remClass.getName()))
-        {
-            Classes.remove(classList);
-            return "Class was successfully removed!";
+            if(classList.getName().equals(remClass.getName()))
+            {
+                Classes.remove(classList);
+                return "Class was successfully removed!";
+            }
         }
-    }
         return "No class matching that name was found.";
+    }
+    public String renameClass(ClassBase renClass)
+    {
+        return "Placeholder!";
     }
 }
