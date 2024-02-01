b885e70d6276ff8323af8628225adf864db4fe49
Cullenisnthere <cullen@kurtzpad.com>
2024-02-01 12:01:00 -0500

    Added in some new tests.

--- ClassTesting.java
+++ ClassTesting.java
@@ -5,30 +5,33 @@ public class ClassTesting
 {
     public static void main(String[] args)
     {
+        ClassBase test = new ClassBase("testClass");
+        ClassContainer classList = new ClassContainer();
         System.out.println("true means it passed the test, false means it failed.");
-        System.out.println("Basic add " + addTest());
+        System.out.println("Basic add " + addTest(test, classList));
         System.out.println("No name add " + addTestNoName());
-        System.out.println("Exists add " + addTestExists());
+        System.out.println("Exists add " + addTestExists(test, classList));
         System.out.println("Basic remove " + removeTest());
         System.out.println("No name remove " + removeTestNoName());
         System.out.println("Doesn't exist remove " + removeTestNotExist());
         System.out.println("Has relationships remove " + removeTestRelationships());
-        ClassBase test = new ClassBase("testClass");
-        ClassContainer classList = new ClassContainer();
-        System.out.println(classList.addClass(test));
-        System.out.println(test.getName());
-        System.out.println(classList.removeClass(test));
     }
-    public static boolean addTest()
+    public static boolean addTest(ClassBase test, ClassContainer classList)
     {
+        String addReply = classList.addClass(test);
+        if(addReply.equals("Class testClass was added."))
+            return true;
         return false;  
     }
     public static boolean addTestNoName()
     {
         return false;
     }
-    public static boolean addTestExists()
+    public static boolean addTestExists(ClassBase test, ClassContainer classList)
     {
+        String addReply = classList.addClass(test);
+        if(addReply.equals("Class with that name already exists!"))
+            return true;
         return false;
     }
     public static boolean removeTest()
