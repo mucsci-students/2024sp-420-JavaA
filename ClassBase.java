96fddeee32ec3826b3c6b9e944536f8b280f3b87
Cullenisnthere <cullen@kurtzpad.com>
2024-02-02 11:10:51 -0500

    Edited ClassBase to let relationship stuff be handled by the relationship file.

--- ClassBase.java
+++ ClassBase.java
@@ -5,18 +5,11 @@ public class ClassBase
     private String className;
     //ArrayList of attributes for this class.
     private ArrayList<attributes> classAttributes;
-    //Ingoing relationship.
-    private ArrayList<Relationship> ingoingRelationships;
-    //ArrayList of outgoing relationships for the class.
-    private ArrayList<Relationship> outgoingRelationships;
     
     public ClassBase(String name)
     {
         className = name;
         classAttributes = new ArrayList<attributes>();
-        ingoingRelationships = new ArrayList<Relationship>();
-        outgoingRelationships = new ArrayList<Relationship>();
-
     }
     public String getName()
     {
@@ -26,21 +19,10 @@ public class ClassBase
     {
         className = newName;
     }
-
-    public void addIngoingRelationship()
-    {
-
-    }
-    public void addOutgoingRelationship()
-    {
-
-    }
     public ClassBase setEquals(ClassBase input)
     {
         input.className = className;
         input.classAttributes = classAttributes;
-        input.ingoingRelationships = ingoingRelationships;
-        input.outgoingRelationships = outgoingRelationships;
         return input;
     }
 
