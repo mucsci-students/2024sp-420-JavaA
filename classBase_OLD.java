bf64c2c7f37286e2b2ffe6ac6a4a988d067b83e7
BasilrockMU <70165455+BasilrockMU@users.noreply.github.com>
2024-02-02 11:28:15 -0500

    Create classBase_OLD.java
    I have both a classBase_OLD which I want to delete and the correctly named ClassBase.java which has the code I was trying to upload earlier.

---
+++ classBase_OLD.java
@@ -0,0 +1,28 @@
+/*public class ClassBase
+{
+    //Name of the class.
+    private String className;
+    //Array of attributes for this class.
+    //private attributes[] classAttributes;
+    //Ingoing relationship.
+    private Relationship ingoingRelationship;
+    //Array of outgoing relationships for the class.
+    private Relationship[] outgoingRelationships;
+    
+    public ClassBase(String name)
+    {
+        className = name;
+        ingoingRelationship = null;
+        outgoingRelationships = null;
+    }
+
+    public void setName(String name)
+    {
+        className = name;
+    }
+
+    public String getName(String name)
+    {
+        return className;
+    }
+}*/
