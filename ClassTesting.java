97fd9ee96b81a9d236fd80d672f5de9e6c28c2c9
Cullenisnthere <cullen@kurtzpad.com>
2024-02-02 10:48:40 -0500

    Finished up rename tests.

--- ClassTesting.java
+++ ClassTesting.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 //Written by: Cullen Kurtz
 //Note to future self:
 //Anything related to not inputting name is automatically handled by ClassContainer
-//methods requiring a ClassBase object, which requires a name to be created.
+//methods requiring a string.
 public class ClassTesting
 {
     public static void main(String[] args)
@@ -15,6 +15,12 @@ public class ClassTesting
         System.out.println("Basic remove " + removeTest(test, classList));
         System.out.println("Doesn't exist remove " + removeTestNotExist(test, classList));
         System.out.println("Has relationships remove " + removeTestRelationships());
+        ClassBase testTwo = new ClassBase("testing");
+        classList.addClass(testTwo);
+        System.out.println("Basic rename " + renameTest(classList, "testing", "newName"));
+        System.out.println("Rename where original doesn't exist " + renameNotExist(classList, "nothingExists", "newNameAgain"));
+        classList.addClass(test);
+        System.out.println("Rename where new name already exists " + renameAlreadyExist(classList, "newName", "testClass"));
     }
     public static boolean addTest(ClassBase test, ClassContainer classList)
     {
@@ -32,14 +38,14 @@ public class ClassTesting
     }
     public static boolean removeTest(ClassBase test, ClassContainer classList)
     {
-        String removeReply = classList.removeClass(test);  
+        String removeReply = classList.removeClass(test.getName());  
         if(removeReply.equals("Class was successfully removed!"))
             return true; 
         return false;
     }
     public static boolean removeTestNotExist(ClassBase test, ClassContainer classList)
     {
-        String removeReply = classList.removeClass(test);
+        String removeReply = classList.removeClass(test.getName());
         if(removeReply.equals("No class matching that name was found."))
             return true;
         return false;
@@ -48,16 +54,25 @@ public class ClassTesting
     {
         return false;
     }
-    public static boolean renameTest()
+    public static boolean renameTest(ClassContainer classList, String testClassName, String newName)
     {
+        String renameReply = classList.renameClass(testClassName, newName);
+        if(renameReply.equals("Class " + testClassName + " was successfully renamed to " + newName + "."))
+            return true;
         return false;
     }
-    public static boolean renameNotExist()
+    public static boolean renameNotExist(ClassContainer classList, String noName, String newName)
     {
+        String renameReply = classList.renameClass(noName, newName);
+        if(renameReply.equals("You can't rename a class that doesn't exist!"))
+            return true;
         return false;
     }
-    public static boolean renameAlreadyExist()
+    public static boolean renameAlreadyExist(ClassContainer classList, String testClassName, String existName)
     {
+        String renameReply = classList.renameClass(testClassName, existName);
+        if(renameReply.equals("A class with your new name already exists!"))
+            return true;
         return false;
     }
 }
