eebb015c6c7cba2b0f01b66d5537f8f7b4a968eb
jrlease-03 <jrlease@millersville.edu>
2024-02-05 14:34:51 -0500

    Most Attribute tasks
    removed class name from attributes class because it seems unneeded, added some attribute methods to classBase

--- attributes.java
+++ attributes.java
@@ -3,22 +3,18 @@ public class attributes <E> {
     //Name for the attribute
     private String name;
 
-    //Name for the class tied to this attribute
-    private String className;
-
     //content of the attribute
     private E content;
 
-    public attributes(newName, newClassName, newContent){
+    public attributes(newName, newContent){
         this.name = newName;
-        this.className = newClassName;
+
         this.content = newContent;
     }
 
     //Used by other classes / users to change the name of this attribute
-    public String setName(String newName){
+    public void setName(String newName){
         this.name = newName;
-        return "name for the attribute was updated";
     }
 
     //will return the name of this attribute
@@ -27,9 +23,8 @@ public class attributes <E> {
     }
 
     //Change the content of the attribute
-    public String setContent(E newContent){
+    public void setContent(E newContent){
         this.content = newContent;
-        return "content for " + this.getName() + " has been updated";
     }
 
     //returns the content of the attribute
