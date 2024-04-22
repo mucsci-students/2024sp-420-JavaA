e6064c965cbe53b429ed22eeff2e71d11acdee20
ramgurung7743 <156702112+ramgurung7743@users.noreply.github.com>
2024-04-21 22:30:28 -0400

    Update MementoStateTest.java
    Added more test codes for this class as well

--- Java2/src/test/java/com/classuml/Model/MementoStateTest.java
+++ Java2/src/test/java/com/classuml/Model/MementoStateTest.java
@@ -6,11 +6,13 @@ import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.Set;
 
 import org.junit.Test;
 
 public class MementoStateTest {
-        @Test
+
+	@Test
 	public void Classes() {
 		ClassBase c = new ClassBase("test");
 		SortedSet<Parameter> p1 = new TreeSet<Parameter>();
@@ -44,4 +46,25 @@ public class MementoStateTest {
 		model3.setBackup(classes);
 		assertEquals(classes, model3.getBackup());
 	}
+	@Test
+    	public void testGetClassNames() {
+        	ClassBase class1 = new ClassBase("Class1");
+        	ClassBase class2 = new ClassBase("Class2");
+        	ClassBase class3 = new ClassBase("Class3");
+
+        	SortedMap<String, ClassBase> classesMap = new TreeMap<>();
+        	classesMap.put("Class1", class1);
+        	classesMap.put("Class2", class2);
+        	classesMap.put("Class3", class3);
+
+        	MementoState mementoState = new MementoState(classesMap);
+        	Set<String> classNames = mementoState.getClassNames();
+        	assertEquals(3, classNames.size());
+
+        	assertTrue(classNames.contains("Class1"));
+        	assertTrue(classNames.contains("Class2"));
+        	assertTrue(classNames.contains("Class3"));
+    	}
+
 }
+
